7929477ec0d9d4cc0c5dd67e02104407d06cae31
thrashches <thrashches@yandex.ru>
2022-11-25 23:54:29 +0300

    comment class

--- index.ts
+++ index.ts
@@ -4,6 +4,17 @@
 class PostComment {
     userId: number;
     text: string;
-    published: Date;
-    
+    published: number;
+    rating: number = 0;
+
+    constructor(userId: number, text: string, published?: number) {
+        if (published) {
+            this.published = published;
+        }
+        else {
+            this.published = Date.now();
+        }
+        this.text = text;
+        this.userId = userId;
+    }
 }
